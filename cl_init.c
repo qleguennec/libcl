297048ec4847693bda3cc1aa394bd7121ef9e416
Quentin Le Guennec <quentin.leguennec1@gmail.com>
2016-12-03 02:46:28 +0000

    fix cl_init

--- cl_init.c
+++ cl_init.c
@@ -6,7 +6,7 @@
 /*   By: qle-guen <qle-guen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/09/11 15:18:43 by qle-guen          #+#    #+#             */
-/*   Updated: 2016/12/02 23:26:09 by qle-guen         ###   ########.fr       */
+/*   Updated: 2016/12/03 02:46:04 by qle-guen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,8 +15,7 @@
 void
 cl_init
 	(t_cl_info *cl
-	 , void *data
-	 , size_t data_size)
+	 , size_t alloc_size)
 {
 	clGetPlatformIDs(1, &cl->pl_id, NULL);
 	clGetDeviceIDs(cl->pl_id
@@ -24,9 +23,6 @@ cl_init
 	cl->ctxt = clCreateContext(NULL, 1, &cl->dev_id, NULL, NULL, NULL);
 	cl->cmd_queue = clCreateCommandQueueWithProperties(cl->ctxt,
 		cl->dev_id, NULL, NULL);
-	if (data)
-	{
-		cl->mem = clCreateBuffer(cl->ctxt
-			, CL_MEM_READ_ONLY | CL_MEM_COPY_HOST_PTR, data_size, data, NULL);
-	}
+	cl->mem = clCreateBuffer(cl->ctxt
+		, CL_MEM_READ_WRITE, alloc_size, NULL, NULL);
 }
