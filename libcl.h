056a3c3794532e1baf7e976c93b715f113dbcac0
Quentin Le Guennec <quentin.leguennec1@gmail.com>
2016-12-03 03:21:11 +0000

    fix header

--- libcl.h
+++ libcl.h
@@ -6,7 +6,7 @@
 /*   By: qle-guen <qle-guen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/09/11 18:24:58 by qle-guen          #+#    #+#             */
-/*   Updated: 2016/12/03 02:46:27 by qle-guen         ###   ########.fr       */
+/*   Updated: 2016/12/03 03:21:08 by qle-guen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,7 +34,7 @@ typedef struct			s_cl_info
 
 cl_event				*cl_krl_exec(t_cl_info *cl, cl_kernel krl, cl_uint work_dim, size_t *work_size);
 cl_kernel				cl_krl_build(t_cl_info *cl, int fd, char *krlname, size_t alloc_size);
-void					cl_init(t_cl_info *cl, size_t data_size);
+void					cl_init(t_cl_info *cl);
 void					cl_write(t_cl_info *cl, size_t offset, void *data, size_t size);
 void					cl_read(t_cl_info *cl, cl_event *ev, void *data, size_t size);
 
